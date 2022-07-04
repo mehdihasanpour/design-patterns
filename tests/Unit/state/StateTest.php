e720f5ad8077ffaf545308ecce33630d516b6475
mehdi hasanpour <me.hasani1367@gmail.com>
2022-07-04 15:04:25 +0430

    modify test

--- tests/Unit/state/StateTest.php
+++ tests/Unit/state/StateTest.php
@@ -14,26 +14,26 @@ class StateTest extends CustomTestCase
     public function dataProvider()
     {
         return [
-            [
-            'canvasType'=> new brushCanvas,
-            'result'=> 'brush'
+            'brush' => [
+                'canvasType' => new brushCanvas,
+                'result' => 'brush'
             ],
-            [
-            'canvasType'=> new selectionCanvas,
-            'result'=> 'selection'
+            'selection' => [
+                'canvasType' => new selectionCanvas,
+                'result' => 'selection'
             ],
-            [
-            'canvasType'=> new eraseCanvas,
-            'result'=> 'erase'
+            'erase' => [
+                'canvasType' => new eraseCanvas,
+                'result' => 'erase'
             ],
         ];
     }
     /**
      * @dataProvider dataProvider
      */
-    public function test_we_should_see_proper_response($canvasType,$result)
+    public function test_we_should_see_proper_response($canvasType, $result)
     {
         $canvas = new canvas($canvasType);
-        $this->assertSame($result,$canvas->mouseUp());
+        $this->assertSame($result, $canvas->mouseUp());
     }
 }
