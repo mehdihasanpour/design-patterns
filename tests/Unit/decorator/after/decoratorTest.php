249cf6528383675e465cfe93428f6074ff6700f2
mehdi hasanpour <me.hasani1367@gmail.com>
2022-07-29 15:37:58 +0430

    add new test for compressed and encrypte togther

--- tests/Unit/decorator/after/decoratorTest.php
+++ tests/Unit/decorator/after/decoratorTest.php
@@ -5,25 +5,33 @@ namespace Test\Unit\decorator\after;
 use App\decorator\cloudstream\after\CloudStream;
 use App\decorator\cloudstream\after\CompressedCloudStream;
 use App\decorator\cloudstream\after\EncryptedCloudStream;
+use App\decorator\cloudstream\after\Stream;
 use Test\Support\CustomTestCase;
 
 class decoratorTest extends CustomTestCase
 {
     public function test_cloud_stream_should_be_able_to_write_data()
     {
-        $cloudStream = new CloudStream();
-        $this->assertSame('mehdi stored in cloud', $cloudStream->write('mehdi'));
+        $this->assertSame('mehdi stored in cloud', $this->storeCreditCard(new CloudStream,'mehdi'));
     }
 
     public function test_cloud_stream_should_be_able_to_encrypt_and_write_data()
     {
-        $encryptedCloudStream = new EncryptedCloudStream(new CloudStream());
-        $this->assertSame('encrypted mehdi stored in cloud', $encryptedCloudStream->write('mehdi'));
+        $this->assertSame('encrypted mehdi stored in cloud', $this->storeCreditCard(new EncryptedCloudStream(new CloudStream()),'mehdi'));
     }
-
+ 
     public function test_cloud_stream_should_be_able_to_compress_and_write_data()
     {
-        $encryptedCloudStream = new CompressedCloudStream(new CloudStream);
-        $this->assertSame('compressed mehdi stored in cloud', $encryptedCloudStream->write('mehdi'));
+        $this->assertSame('compressed mehdi stored in cloud', $this->storeCreditCard(new CompressedCloudStream(new CloudStream()),'mehdi'));
+    }
+
+    public function test_cloud_stream_should_be_able_to_compress_and_encrypt_and_write_data()
+    {
+        $this->assertSame('compressed encrypted mehdi stored in cloud', $this->storeCreditCard(new EncryptedCloudStream(new CompressedCloudStream(new CloudStream())),'mehdi'));
+    }
+
+    private function storeCreditCard(Stream $stream,string $data)
+    {
+        return $stream->write($data);
     }
 }
